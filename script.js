742f8ac0be0fcc4b3f87b1f54ae640c2dd82f06c
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-03-21 14:54:49 +0100

    init project

---
+++ script.js
@@ -0,0 +1,8 @@
+const { multiply, add } = require("./math");
+
+console.log(multiply(2, 3));
+console.log(add(2, 3));
+
+var kebabCase = require("kebab-case");
+
+console.log(kebabCase("ViveLesTacos"));
